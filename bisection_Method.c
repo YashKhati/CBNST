4968e64fbcb37fd0be1bdb77f4e5a5559902450f
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2022-12-22 22:38:02 +0530

    Update bisection_Method.c

--- bisection_Method.c
+++ bisection_Method.c
@@ -1,4 +1,58 @@
 #include<stdio.h>
+#include<math.h>
+float fun(float x)
+{
+    return (x*log10(x) - 1.2);
+}
+int main()
+{
+    float a,b;
+    while(1)
+    {
+        printf("Enter value of xo and x1 : \n");
+        printf("Xo = ");
+        scanf("%f",&a);
+        printf("X1 = ");
+        scanf("%f",&b);
+        if(fun(a)*fun(b) < 0)
+        {
+            printf("Roots lies between %0.2f and %0.2f\n",a,b);
+            break;
+        }
+        printf("\nInvalid Roots\n");
+        printf("\nEnter values again \n");
+    }
+    float tol;
+    int i = 1 ;
+    printf("Enter tollerance value : ");
+    scanf("%f",&tol);
+    float x3,x;
+    do
+    {
+        x = (a+b)/2.0;
+        if(fun(a)*fun(x) < 0 )
+        {
+            b = x;
+        }   
+        else
+        {
+            a=x;
+        }
+        printf("Itteration : %d\tx : %f\tf(x) : %f\n",i,x,fun(x));
+        if(fabs(b-a)<tol)
+        {
+            break;
+        }
+        i++;
+    } while (fabs(b-a)>tol);
+    
+    printf("Roots are : %f Total Iterations: %d ",x,i);
+    return 0;
+}
+
+// 2 way
+
+#include<stdio.h>
 float findValueAt(float x)
 {
    return x*x*x - 2*x -5;
